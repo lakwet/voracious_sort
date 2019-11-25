8c0921a270fc255d2ce9ec697f6c753c750e6234
AxellePiotFretlink <axelle.piot@fretlink.com>
2019-11-25 14:24:45 +0100

    Add Thiel sort

---
+++ src/sorts/thiel_sort.rs
@@ -0,0 +1,185 @@
+use super::super::algo::k_way_merge::k_way_merge;
+use super::super::algo::verge_sort_heuristic::verge_sort_preprocessing;
+use super::super::{Radixable, RadixableForContainer};
+use super::lsd_sort::lsd_radixsort_body;
+use super::msd_sort::copy_by_histogram;
+use super::utils::{
+    copy_nonoverlapping, get_full_histogram_except_for_last_level,
+    only_one_bucket_filled, prefix_sums, swap, Params,
+};
+
+pub fn thiel_radixsort_body<T>(arr: &mut [T], p: Params)
+where
+    T: Radixable<KeyType = <[T] as RadixableForContainer>::KeyType>
+        + Copy
+        + PartialOrd,
+    [T]: RadixableForContainer,
+{
+    if arr.len() <= 128 {
+        arr.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap());
+        return;
+    }
+
+    let size = arr.len();
+    let histograms = get_full_histogram_except_for_last_level(arr, &p);
+
+    let estimated_size = ((size >> p.radix) + size % p.radix + 1) as usize;
+    let mut buffer: Vec<T> = vec![arr[0]; estimated_size * p.radix_range];
+
+    let t1 = arr;
+    let t2 = &mut buffer;
+    let t2 = t2.as_mut_slice();
+
+    let mut source = t1;
+    let destination = t2;
+
+    /*** first pass with estimated buckets size ***/
+    let mut first_histogram = vec![0; p.radix_range];
+    let mut overflow_histogram = vec![0; p.radix_range];
+    let mut estimated_heads = Vec::with_capacity(p.radix_range);
+    for i in 0..p.radix_range {
+        estimated_heads.push(i * estimated_size);
+    }
+    let mut estimated_tails = estimated_heads.to_vec();
+    estimated_tails.push(p.radix_range * estimated_size);
+    estimated_tails.remove(0);
+
+    let (mask, shift) =
+        source.get_mask_and_shift(&p.new_level(p.max_level - 1));
+    let mut overflow_index = 0;
+
+    for i in 0..size {
+        let target_bucket = source[i].get_key(mask, shift);
+        first_histogram[target_bucket] += 1;
+        // if the bucket buffer is not full, put the element in the buffer
+        if estimated_heads[target_bucket] < estimated_size {
+            destination[estimated_heads[target_bucket]] = source[i];
+            estimated_heads[target_bucket] += 1;
+        } else {
+            // otherwise use the source input array
+            swap(&mut source, i, overflow_index);
+            overflow_histogram[target_bucket] += 1;
+            overflow_index += 1;
+        }
+    }
+
+    /*** second LSD radix sort pass and to handle overflow elements ***/
+    // copy overflow element in another bucket...
+    let mut overflow_buckets = Vec::<T>::new();
+    let (_, mut overflow_heads, overflow_tails) =
+        prefix_sums(&overflow_histogram);
+    let overflow_heads_copy = overflow_heads.to_vec();
+    if overflow_index > 0 {
+        overflow_buckets = vec![destination[0]; overflow_index];
+        for item in source.iter().take(overflow_index) {
+            let target_bucket = item.get_key(mask, shift);
+            overflow_buckets[overflow_heads[target_bucket]] = *item;
+            overflow_heads[target_bucket] += 1;
+        }
+    }
+
+    // perform second pass while taking into account overflow_buckets
+    let level = p.max_level - 2;
+    let (mask, shift) = source.get_mask_and_shift(&p.new_level(level));
+    let (_, mut heads, _) = prefix_sums(&histograms[level]);
+
+    for prev_bucket in 0..p.radix_range {
+        for item in overflow_buckets
+            .iter()
+            .take(overflow_tails[prev_bucket])
+            .skip(overflow_heads_copy[prev_bucket])
+        {
+            let target_bucket = item.get_key(mask, shift);
+            source[heads[target_bucket]] = *item;
+            heads[target_bucket] += 1;
+        }
+        for item in destination
+            .iter()
+            .take(estimated_heads[prev_bucket])
+            .skip(prev_bucket * estimated_size)
+        {
+            let target_bucket = item.get_key(mask, shift);
+            source[heads[target_bucket]] = *item;
+            heads[target_bucket] += 1;
+        }
+    }
+
+    let mut t1 = source;
+    let mut t2 = destination;
+
+    /*** then use regular LSD radix sort ***/
+    let mut index = 0;
+    for level in (0..(p.max_level - 2)).rev() {
+        if only_one_bucket_filled(&histograms[level]) {
+            continue;
+        }
+
+        let (mut source, mut destination) =
+            if index == 0 { (t1, t2) } else { (t2, t1) };
+        let (mask, shift) = source.get_mask_and_shift(&p.new_level(level));
+        let (_, mut heads, _) = prefix_sums(&histograms[level]);
+
+        copy_by_histogram(
+            size,
+            &mut source,
+            &mut destination,
+            &mut heads,
+            mask,
+            shift,
+        );
+
+        index = 1 - index;
+
+        if index == 1 {
+            t1 = source;
+            t2 = destination;
+        } else {
+            t2 = source;
+            t1 = destination;
+        }
+    }
+
+    if index == 1 {
+        copy_nonoverlapping(t2, t1, size);
+    }
+}
+
+fn thiel_radixsort_aux<T>(arr: &mut [T], radix: usize)
+where
+    T: Radixable<KeyType = <[T] as RadixableForContainer>::KeyType>
+        + Copy
+        + PartialOrd,
+    [T]: RadixableForContainer<T = T>,
+{
+    if arr.len() <= 128 {
+        arr.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap());
+        return;
+    }
+
+    let (offset, _) = arr.compute_offset(radix);
+    let max_level = arr.compute_max_level(offset, radix);
+    let params = Params::new(0, radix, offset, max_level);
+
+    if params.max_level < 2 {
+        lsd_radixsort_body(arr, params);
+    } else {
+        thiel_radixsort_body(arr, params);
+    }
+}
+
+pub fn thiel_radixsort<T>(arr: &mut [T], radix: usize)
+where
+    T: Radixable<KeyType = <[T] as RadixableForContainer>::KeyType>
+        + Copy
+        + PartialOrd,
+    [T]: RadixableForContainer<T = T>,
+{
+    if arr.len() <= 128 {
+        arr.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap());
+        return;
+    }
+
+    let mut separators =
+        verge_sort_preprocessing(arr, radix, &thiel_radixsort_aux);
+    k_way_merge(arr, &mut separators);
+}
