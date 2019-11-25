4b1799bf1ab717d580ec7767af977b74ac334cf8
AxellePiotFretlink <axelle.piot@fretlink.com>
2019-11-25 14:23:42 +0100

    Add sorts module

---
+++ src/sorts/mod.rs
@@ -0,0 +1,12 @@
+pub mod american_flag_sort;
+pub mod boolean_sort;
+pub mod comparative_sort;
+pub mod counting_sort;
+pub mod dlsd_sort;
+pub mod lsd_sort;
+pub mod msd_sort;
+pub mod msd_string_sort;
+pub mod ska_sort;
+pub mod thiel_sort;
+pub mod utils;
+pub mod voracious_sort;
